cdbae710c194ded64c399f0592f6e4dc809fdc5b
Nicholas Griffin <bgriffin@student.neumont.edu>
2026-08-24 23:25:23 -0600

    genuinely fml

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const database = require("./database");
+const database = require("./Database");
 
 const app = express();
 
--- api/getuser.js
+++ api/getuser.js
@@ -1,4 +1,4 @@
-const database = require("../database");
+const database = require("../Database");
 
 module.exports = async (req, res) => {
     try {
