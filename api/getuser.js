91edc2ec6421cfa4cf4f6fe10b90a112fe2d3903
Nicholas Griffin <bgriffin@student.neumont.edu>
2026-08-24 23:21:30 -0600

    idek anymore

--- api/getuser.js
+++ api/getuser.js
@@ -1,25 +1,15 @@
+const database = require("../database");
+
 module.exports = async (req, res) => {
     try {
-        const database = require("../database");
-
-        console.log("Database module loaded");
-
         const users = await database.getUsers();
 
-        console.log("Users retrieved");
-
         return res.status(200).json(users);
-
     } catch (error) {
-        console.error("================================");
-        console.error("GETUSER ERROR");
         console.error(error);
-        console.error("================================");
 
         return res.status(500).json({
-            error: error.message,
-            name: error.name,
-            stack: error.stack
+            error: error.message
         });
     }
 };
