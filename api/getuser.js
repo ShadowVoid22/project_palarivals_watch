2b736a36aabf769924f511d079c96d8da513969c
Nicholas Griffin <bgriffin@student.neumont.edu>
2026-08-25 00:24:28 -0600

    Possibly finally implementing getuser.js?

--- api/getuser.js
+++ api/getuser.js
@@ -1,15 +1,22 @@
 const database = require("../Database");
 
 module.exports = async (req, res) => {
+        
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            error: "Method not allowed"
+        });
+    }
+
     try {
         const users = await database.getUsers();
 
-        return res.status(200).json(users);
+        res.json(users);
     } catch (error) {
         console.error(error);
 
-        return res.status(500).json({
-            error: error.message
+        res.status(500).json({
+            error: "Database error"
         });
     }
 };
