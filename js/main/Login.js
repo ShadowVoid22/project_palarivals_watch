037a7cf3c1911621730f0e360000afc2d7838d4d
Nicholas Griffin <bgriffin@student.neumont.edu>
2026-08-25 02:27:17 -0600

    plz?

--- js/main/Login.js
+++ js/main/Login.js
@@ -20,7 +20,7 @@ async function submitClicked(){
 }
 
 async function tryLogin(){
-    const users = await getUsers(UsernameInput.value);
+    const users = await getUsers("Admin1");
     let username = users.Username;
     console.log(username);
     console.log("PLEASE WORK");
